c47788a78c92b2a99ed3a27b05b949606b56d325
1hakr <heart.break.kid.b4u@gmail.com>
2013-06-06 17:26:17 +0530

    Refactoring phase 1

---
+++ src/dev/dworks/widgets/DateSlider/labeler/MonthLabeler.java
@@ -0,0 +1,27 @@
+package dev.dworks.widgets.DateSlider.labeler;
+
+import java.util.Calendar;
+
+import dev.dworks.widgets.DateSlider.TimeObject;
+
+/**
+ * A Labeler that displays months
+ */
+public class MonthLabeler extends Labeler {
+    private final String mFormatString;
+
+    public MonthLabeler(String formatString) {
+        super(180, 60);
+        mFormatString = formatString;
+    }
+
+    @Override
+    public TimeObject add(long time, int val) {
+        return timeObjectfromCalendar(Util.addMonths(time, val));
+    }
+
+    @Override
+    protected TimeObject timeObjectfromCalendar(Calendar c) {
+        return Util.getMonth(c, mFormatString);
+    }
+}
